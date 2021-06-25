1843a7f89a7b96d32158cf281bb7a26c4917c8f1
Shikhar Singh <shikharsingh.original@gmail.com>
2021-06-26 00:01:43 +0530

    [UPDATED] return data

--- controllers/admin/payments.controller.js
+++ controllers/admin/payments.controller.js
@@ -2,7 +2,7 @@ const Payments = require('../../models/payments.model');
 const User = require('../../models/user.model');
 
 exports.getPaymentsController = async (req,res) => {
-    const status = req?.query?.status;
+    const status = req?.query?.type;
     const eventId = req?.query?.eventId;
 
     let query = {};
@@ -19,15 +19,21 @@ exports.getPaymentsController = async (req,res) => {
         let usersData = [];
         for(let i=0;i<payments.length;i++) {
             const payment = payments[i];
-            const userInfo = await User.findById(payment.user_id,{name: 1,email: 1,created_at: 1});
+            const userInfo = await User.findById(payment.user_id,{name: 1,email: 1,
+                created_at: 1,imageLocation: 1});
             usersData.push(userInfo);
         }
 
         const paymentData = payments.map((item,idx) => {
             return {
                 ...item._doc,
+                amount: (item.amount / 100),
+                amount_paid: (item.amount_paid / 100),
+                amount_due: (item.amount_due / 100),
+                title: item.description.split(': ')[1],
                 name: usersData[idx].name.fname + " " + usersData[idx].name.lname,
                 email: usersData[idx].email,
+                imageLocation: usersData[idx]?.imageLocation,
                 joinedOn: usersData[idx].created_at
             }
         });
